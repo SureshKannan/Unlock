08af4a51bdbaa93f59d646650e036a4e61a8b9fd
SureshKannan <ksuriyaa@hotmail.com>
2014-10-16 08:01:45 -0400

    With user registeration

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,19 +2,21 @@ class CustomersController < ApplicationController
   def index
     @customer = Customer.new
     @countries = Country.all
+    @imei = params["txtImei"]
   end
   def new
     @customer = Customer.new
   end
   def create
-    
+    @customer = Customer.new(customer_params)
+    @customer.save
   end
   private
   def customer_params
     if :customer.nil?
-      params.permit(:id,:email,:firstname,:lastname,:country_id,:imei,:mode)
+      params.permit(:id,:email,:FirstName,:LastName,:Country_id,:imei)
     else
-      params.require(:customer).permit(:id,:email,:firstname,:lastname,:country_id,:imei,:mode)
+      params.require(:customer).permit(:id,:email,:FirstName,:LastName,:Country_id,:imei)
     end
   end
 end
